f4a51d2d1dc39a78efb7aff037962246ceb5839a
Markku Kinnunen <markku.kinnunen@gmail.com>
2018-07-13 23:29:29 +0300

    Add project files.

---
+++ RoslynLib/RoslynLib.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.CodeAnalysis.MSBuild;
+using System.Collections.Immutable;
+
+namespace RoslynLib
+{
+    public class SolutionInfoProvider
+    {
+        private MSBuildWorkspace _MSBuildWorkspace;
+
+        public SolutionInfoProvider()
+        {
+            _MSBuildWorkspace = MSBuildWorkspace.Create(ImmutableDictionary<string, string>.Empty);
+        }
+    }
+}
---
+++ RoslynTest/MainWindow.xaml.cs
@@ -0,0 +1,18 @@
+﻿using RoslynLib;
+using System.Windows;
+
+namespace RoslynTest
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            var solutionInfoProvider = new SolutionInfoProvider();
+        }
+    }
+}
